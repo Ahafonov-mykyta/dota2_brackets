055dc865c52b8b9f8fcd365ac39b2afcf4775b09
Mykyta Ahafonov <103929069+Ahafonov-mykyta@users.noreply.github.com>
2023-08-01 23:05:29 +0300

    my api fixes

--- src/helpers/getHeroName.js
+++ src/helpers/getHeroName.js
@@ -3,7 +3,7 @@ import axios from "axios";
 async function getDotaHeroes() {
   try {
     const response = await axios.get(
-      `https://aqueous-headland-75357-75ce39fd598f.herokuapp.com//api/heroes`
+      `https://aqueous-headland-75357-75ce39fd598f.herokuapp.com/api/heroes`
     );
     const heroesData = response.data.result.heroes;
     return heroesData;
